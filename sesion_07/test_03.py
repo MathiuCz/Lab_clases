9121608786338b2cfdde9676ce8fa4dcd9415278
unknown <mcondezavaleta@gmail.comgit config --global user.name Mathius Conde>
2025-04-12 01:57:30 -0500

    Last clases

---
+++ sesion_07/test_03.py
@@ -0,0 +1,19 @@
+"""
+Uso de librerías de fecha y tiempo
+Conversión total del tiempo
+"""
+
+from datetime import datetime
+
+"""uso del método timestamp"""
+
+time_now = datetime.strptime("2025/05/02 20:30:00",
+                             "%Y/%m/%d %H:%M:%S").timestamp()
+
+#"tantos segundos después de esa fecha".
+"""
+H: Hora
+m: Minuto
+S: Segundo
+"""
+print(time_now)
